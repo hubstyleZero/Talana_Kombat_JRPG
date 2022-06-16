f60fa01fd99d80af3133d80df439adf4bebc5273
Andres Gonzalez <79916717+hubstyleZero@users.noreply.github.com>
2022-06-16 10:36:27 -0400

    V 0.2

--- Validacion.py
+++
@@ -1,7 +0,0 @@
-def validacion_input(input_data, allowed, limit):
-    if len(input_data) > limit:
-        return True
-    elif any(x not in allowed for x in input_data):
-        return True
-    else:
-        return False
